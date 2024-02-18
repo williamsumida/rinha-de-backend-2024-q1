962d64a32213e5d9c39901c26ca8f51178063fe2
William Sumida <william.h.sumida@gmail.com>
2024-02-17 23:26:34 -0300

    setup inicial com rotas

--- src/server.ts
+++ src/server.ts
@@ -16,16 +16,57 @@ const app = fastify({ logger: envToLogger });
 app.post(
   "/clientes/:id/transacoes",
   async (request: FastifyRequest, reply: FastifyReply) => {
-    const clientId = request.params.id;
-    const transactionData = request.body;
+    const clientIdSchema = z.object({ id: z.coerce.number().int() });
+    const transactionDataBodySchema = z.object({
+      valor: z.number().positive(),
+      tipo: z.enum(["c", "d"]),
+      descricao: z.string().min(1).max(10),
+    });
 
-    app.log.info(clientId);
+    const { id } = clientIdSchema.parse(request.params);
+    const { valor, tipo, descricao } = transactionDataBodySchema.parse(
+      request.body,
+    );
+
     const replyData = {
       limite: 100000,
       saldo: -9098,
     };
 
     reply.send(replyData);
+  },
+);
+
+app.get(
+  "/clientes/:id/extrato",
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    const clientIdSchema = z.object({ id: z.coerce.number().int() });
+    const { id } = clientIdSchema.parse(request.params);
+    app.log.info(id);
+
+    const extrato = {
+      saldo: {
+        total: -9098,
+        data_extrato: "2024-01-17T02:34:41.217753Z",
+        limite: 100000,
+      },
+      ultimas_transacoes: [
+        {
+          valor: 10,
+          tipo: "c",
+          descricao: "descricao",
+          realizada_em: "2024-01-17T02:34:38.543030Z",
+        },
+        {
+          valor: 90000,
+          tipo: "d",
+          descricao: "descricao",
+          realizada_em: "2024-01-17T02:34:38.543030Z",
+        },
+      ],
+    };
+
+    reply.send(extrato);
   },
 );
 
