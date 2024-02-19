17082c33dbbce716882a2bb4949e825d268a416a
William Sumida <william.h.sumida@gmail.com>
2024-02-19 17:56:57 -0300

    database structure and initialization

--- config/init.sql
+++ config/init.sql
@@ -2,19 +2,16 @@ CREATE TABLE accounts (
 	id SERIAL UNIQUE NOT NULL,
 	name VARCHAR(50) NOT NULL,
 	limit_amount INTEGER NOT NULL,
-	balance INTEGER NOT NULL
+	balance INTEGER NOT NULL,
+  transactions JSON
 );
 
-CREATE TABLE transactions (
-	id SERIAL PRIMARY KEY,
-	account_id INTEGER NOT NULL,
-	amount INTEGER NOT NULL,
-	transaction_type CHAR(1) NOT NULL,
-	description VARCHAR(10) NOT NULL,
-	date TIMESTAMP NOT NULL DEFAULT NOW(),
-	CONSTRAINT fk_accounts_transactions_id
-		FOREIGN KEY (account_id) REFERENCES accounts(id)
-);
+CREATE INDEX idx_accounts ON accounts(id);
 
-CREATE INDEX idx_accounts ON accounts(id) INCLUDE (limit_amount, balance);
-CREATE INDEX idx_transactions_account_id ON transactions(account_id);
+INSERT INTO accounts (name, limit_amount, balance, transactions)
+VALUES
+  ('a', 1000 * 100, 0, '[]'),
+  ('b', 800 * 100, 0, '[]'),
+  ('c', 10000 * 100, 0, '[]'),
+  ('d', 100000 * 100, 0, '[]'),
+  ('e', 5000 * 100, 0, '[]');
